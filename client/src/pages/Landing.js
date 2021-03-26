bc37c773efbb1a7a6c12b3fb9e2b74b0c1c56eed
chavalk <chavalk@hotmail.com>
2021-03-25 21:03:13 -0500

    Add logo to right side of page.

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../img/logo.png';
+
+const Landing = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+
+                </div>
+                <div className="col-md-6">
+                    <img src={logo} class="img-fluid" alt="Logo"></img>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Landing;
