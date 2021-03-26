19292f9ee026d29b1f411da09acce1f6c2373aaf
Harrison Noblett <HarrisonNoblett55@gmail.com>
2021-03-25 21:56:00 -0500

    added design and basic mvp for the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import react from 'react'
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-   <div>
-     Hello
-   </div>
+  <Router>
+    <div>
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/Dashboard" component={Dashboard} />
+    </div>
+  </Router>
   );
 }
 
