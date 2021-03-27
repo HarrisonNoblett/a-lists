1c8ac06e24cc0354ffdb26e4cfdf1ca8c01a7cf3
chavalk <chavalk@hotmail.com>
2021-03-26 22:57:26 -0500

    Add main picture on left side of landing page and h1.

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from '../img/logo.png';
+import movie from '../img/movie.jpg';
 
 const Landing = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-6">
-
+                <div className="col-md-7">
+                    <img src={movie} className="img-fluid" alt="Movie" style={{ height: '100%'}}></img>
                 </div>
-                <div className="col-md-6">
-                    <img src={logo} class="img-fluid d-block" alt="Logo" ></img>
+                <div className="col-md-5">
+                    <img src={logo} className="img-fluid d-block" alt="Logo" ></img>
+                    <h1 className="mb-3">Join A-Lists today.</h1>
                     <button type="button" className="btn btn-primary d-block mb-3">Sign up</button>
                     <button type="button" className="btn btn-primary d-block">Log in</button>
                 </div>
