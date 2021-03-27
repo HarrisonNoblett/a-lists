a658fd8f3b360959a8b4817b37ca75ed876b45be
chavalk <chavalk@hotmail.com>
2021-03-27 11:57:54 -0500

    Add routes to app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import react from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashbord';
 
 function App() {
   return (
-   <div>
-     Hello
-   </div>
+   <Router>
+     <div>
+       <Switch>
+         <Route exact path='/' component={Landing} />
+         <Route exact path='/login' component={Login} />
+         <Route exact path='/signup' component={Signup} />
+         <Route exact path='/dashboard' component={Dashboard} />
+       </Switch>
+     </div>
+   </Router>
   );
 }
 
