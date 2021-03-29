2a71a27c8eb92bf2f9a5bb3391e7e158ff9894d6
chavalk <chavalk@hotmail.com>
2021-03-28 20:53:15 -0500

    Create netflix page and add route for the page in app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
@@ -6,6 +5,7 @@ import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Signup from './pages/signup';
 import Dashboard from './pages/Dashboard';
+import Netflix from './pages/Netflix';
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
          <Route exact path='/login' component={Login} />
          <Route exact path='/signup' component={Signup} />
          <Route exact path='/dashboard' component={Dashboard} />
+         <Route exact path='/netflix' component={Netflix} />
        </Switch>
      </div>
    </Router>
