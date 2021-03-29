07983e8c8c9d6adf73ca989161c830fb0361d65b
Harrison Noblett <HarrisonNoblett55@gmail.com>
2021-03-28 19:00:57 -0500

    working on the dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -11,59 +11,76 @@ import "./style.css";
 function Dashboard() {
     return (
         <div>
-            <nav class="navbar navbar-dark bg-transparent">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">
-                    <img src={logo} alt="A-Lists" width="150" class="d-inline-block align-text-top"/>
-                    </a>
-                </div>
-            </nav>
-            <div className="container">
+            <div className="d-flex flex-column float-left text-white bg-dark rounded shadow-xl">
+                <ul className="nav nav-pills nav-flush flex-column mb-auto text-center">
+                    <div className="container-fluid text-center">
+                        <a className="navbar-brand" href="#">
+                        <img src={logo} alt="A-Lists" width="200" className="d-inline-block align-text-top"/>
+                        </a>
+                    </div>
+                    <hr />
+                    <li className="nav-item">
+                        <a className="nav-link active py-3 border-bottom" title data-bs-toggle="tooltip" aria-current="page" data-bs-placement="right" href="#">Dahsboard</a>
+                    </li>
+                    <hr />
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Login</a>
+                    </li>
+                    <hr />
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Signup</a>
+                    </li>
+                    <hr />
+                </ul>
+            </div>
+            <div className="d-flex flex-column float-right text-white bg-dark rounded shadow-xl" width="500">
+                <ul className="nav nav-pills nav-flush flex-column m-3 text-center">
+                    <div className="container-fluid text-center">
+                        Favorites
+                    </div>
+                    <hr />
+                    <li className="service" id="service-item">
+                        <h5 class="card-title fw-bold text-white">Netflix</h5>
+                    </li>
+                    <hr />
+                    <li className="info shadow-lg m-3 p-3">
+                        <img src="https://gortoncenter.org/wp-content/uploads/2018/03/LordOfTheRings.jpg" width="350" height="200" alt="Movie Image"/>
+                            <br />                    
+                        <p className="fw-bold">The Lord of the Rings is the saga of a group of sometimes reluctant heroes who set forth to save their world from consummate evil. Its many worlds and creatures were drawn from Tolkien's extensive knowledge of philology and folklore.</p>
+                    </li>
+                    <hr />
+                    <div className="text-primary">
+                        A-lists
+                    </div>
+                    <hr />
+                </ul>
+            </div>
+            <div className="container-lg">
                 <div className="row">
-                    <div className="col-md-4">
-                        <button type="button" class="btn m-5 btn-lg rounded shadow-lg"><img src={Netflix} alt="Netflix" width="250" height="150"/></button>
+                    <div className="row">
+                        <div className="col-md-4">
+                            <button type="button" class="btn m-5 btn-lg rounded shadow-lg"><img src={Netflix} alt="Netflix" width="250" height="150"/></button>
+                        </div>
+                        <div className="col-md-4">
+                            <button type="button" class="btn m-5 btn-lg rounded shadow-lg"><img src={Hulu} alt="Hulu" width="250" height="150"/></button>
+                        </div>
+                        <div className="col-md-4">
+                            <button type="button" class="btn m-5 btn-lg rounded shadow-lg"><img src={Disney} alt="Disney" width="250" height="150"/></button>
+                        </div>
                     </div>
-                    <div className="col-md-4">
-                        <button type="button" class="btn m-5 btn-lg rounded shadow-lg"><img src={Hulu} alt="Hulu" width="250" height="150"/></button>
-                    </div>
-                    <div className="col-md-4">
-                        <button type="button" class="btn m-5 btn-lg rounded shadow-lg"><img src={Disney} alt="Disney" width="250" height="150"/></button>
-                    </div>
-                    
-                </div>
-                <div className="row">
-                    <div className="col-md-4">
-                        <button type="button" class="btn m-5 btn-lg rounded shadow-lg"><img src={HBO} alt="HBO-Max" width="250" height="150"/></button>
-                    </div>
-                    <div className="col-md-4">
-                        <button type="button" class="btn m-5 btn-lg rounded shadow-lg"><img src={Prime} alt="Amazon Prime Video" width="250" height="150"/></button>
-                    </div>
-                    <div className="col-md-4">
-                        <button type="button" class="btn m-5 btn-lg rounded shadow-lg"><img src={Apple} alt="Apple tv" width="250" height="150"/></button>
+                    <div className="row">
+                        <div className="col-md-4">
+                            <button type="button" class="btn m-5 btn-lg rounded shadow-lg"><img src={HBO} alt="HBO-Max" width="250" height="150"/></button>
+                        </div>
+                        <div className="col-md-4">
+                            <button type="button" class="btn m-5 btn-lg rounded shadow-lg"><img src={Prime} alt="Amazon Prime Video" width="250" height="150"/></button>
+                        </div>
+                        <div className="col-md-4">
+                            <button type="button" class="btn m-5 btn-lg rounded shadow-lg"><img src={Apple} alt="Apple tv" width="250" height="150"/></button>
+                        </div>
                     </div>
                 </div>
             </div>
-                <div className="row">
-                    <div class="card text-center bg-dark text-white m-5">
-                        <div class="card-header">
-                            Favorites
-                        </div>
-                            <div class="card-body">
-                                <h5 class="card-title fw-bold text-primary">Streaming Service</h5>
-                                <div className="row m-2">
-                                    <div className="col-md-6">
-                                        <img src="https://gortoncenter.org/wp-content/uploads/2018/03/LordOfTheRings.jpg" width="350" height="200" alt="Movie Image"/>
-                                    </div>
-                                    <div className="col-md-6 ">
-                                        <p className="fw-bold">The Lord of the Rings is the saga of a group of sometimes reluctant heroes who set forth to save their world from consummate evil. Its many worlds and creatures were drawn from Tolkien's extensive knowledge of philology and folklore.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        <div class="card-footer text-muted">
-                            A-Lists
-                        </div>
-                    </div>
-                </div>
         </div>
     )
 }
