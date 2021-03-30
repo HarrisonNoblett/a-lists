f0ea1f47a4bcad82aa46d1aebdee2eee2a7d51bf
chavalk <chavalk@hotmail.com>
2021-03-30 16:54:17 -0500

    Create components folder and form folder to use for input and form button. Set initial state for object to store form input.

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../img/logo.png';
+import { Input, FormBtn } from "../components/Form";
 
 const Signup = () => {
+    // Setting component's initial state
+    const [formObject, setFormObject] = useState({})
+    
     return (
         <div className="container">
             <div className="row">
@@ -11,13 +15,23 @@ const Signup = () => {
                 <div className="col-md-4">
                     <img src={logo} className="img-fluid" alt="Logo"></img>
                     <h2 className="mb-3">Create your account</h2>
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"></input>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1"></input>
-                    </div>
-                    <a href="/login" className="btn btn-primary d-block">Sign up</a>
+                    <form>
+                        <Input
+                            name="name"
+                            placeholder="Name"
+                        />
+                        <Input
+                            name="email"
+                            placeholder="Email"
+                        />
+                        <Input
+                            name="password"
+                            placeholder="Password"
+                        />
+                        <FormBtn>
+                            Sign up
+                        </FormBtn>
+                    </form>
                 </div>
                 <div className="col-md-4">
 
