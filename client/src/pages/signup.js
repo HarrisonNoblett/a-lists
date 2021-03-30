f433baa8b71ff0be7abf1ae5284156fcab43c567
chavalk <chavalk@hotmail.com>
2021-03-30 17:03:27 -0500

    Add handle input change function to capture and set state for each of the input fields.

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -5,7 +5,13 @@ import { Input, FormBtn } from "../components/Form";
 const Signup = () => {
     // Setting component's initial state
     const [formObject, setFormObject] = useState({})
-    
+
+    // Handles updating component state when the user types into the input field
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setFormObject({...formObject, [name]: value})
+    };
+
     return (
         <div className="container">
             <div className="row">
@@ -17,14 +23,17 @@ const Signup = () => {
                     <h2 className="mb-3">Create your account</h2>
                     <form>
                         <Input
+                            onChange={handleInputChange}
                             name="name"
                             placeholder="Name"
                         />
                         <Input
+                            onChange={handleInputChange}
                             name="email"
                             placeholder="Email"
                         />
                         <Input
+                            onChange={handleInputChange}
                             name="password"
                             placeholder="Password"
                         />
