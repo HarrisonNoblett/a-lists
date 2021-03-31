35327680804ba40ea0cd1c76e21be7c0d360f66e
Janneth Verduzco <janneth_verduzco@live.com>
2021-03-30 20:58:21 -0500

    1) added networkSeed(scripts) for HBO Max to start with. 2)Added watchList.js model. 3) Added index.js in the the models folder to export model files

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 export default {
-    // Gets all books
-    getFilms: function (film) {
-        return axios.get("/api/films/" + film);
-    },
-
+  // Gets all books
+  getFilms: function (film) {
+    return axios.get("/api/films/" + film);
+  },
+  getNetwork: function (network) {
+    return axios.get("/api/netwrok/" + network);
+  },
 };
