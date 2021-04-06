9acfc73668da1d42fd8f62cf926ae2d31ce3e8d4
Gabriele Falloon <ggfalloon@gmail.com>
2021-04-06 09:52:28 -0500

    styling and content to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,11 +1,33 @@
-import React from "react"
-import "./style.css"
+import React from "react";
+import "./style.css";
+import logo from "../../img/logocopy.png";
 
 function Footer() {
     return (
-        <div className="bg-transparent text-white text-center">
-            <h3>A-Lists™️</h3>
-        </div>
+        <footer>
+            <div className="wrapper mx-auto">
+                <div className="row">
+                    <div className="col-sm-5">
+                        <img src={logo} alt="logo"></img><br />
+                        <p>&copy; 2021 All Rights Reserved.</p>
+                    </div>
+                    <div className="col-sm-3">
+                        <h3>A-LISTS TEAM</h3>
+                        <a href="https://github.com/chavalk">Jose Garcia</a><br />
+                        <a href="https://github.com/ggfalloon">Gabriele Falloon</a><br />
+                        <a href="https://github.com/HarrisonNoblett">Harrison Noblett</a><br />
+                        <a href="https://github.com/jannverduzco">Janneth A. Verduzco</a><br />
+                    </div>
+                    <div className="col-sm-4">
+                        <h3>RESOURCES</h3>
+                        <p></p>
+
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+
     )
 }
 
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -5,9 +5,9 @@ import "./style.css"
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-dark fixed-top" style={{ backgroundColor: "rgba(0,0,0,0.8"}}>
+        <nav className="navbar navbar-dark fixed-top" style={{ backgroundColor: "rgba(0,0,0,0.8" }}>
             <a className="navbar-brand" href="/dashboard">
-                <img src={logo} alt="Logo" width="100" height="24"></img>
+                <img src={logo} alt="Logo" width="115"></img>
             </a>
             <LogoutButton />
         </nav>
