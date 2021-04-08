e03982a77a42c486500f30e20187f66d0c8c9d03
Janneth Verduzco <janneth_verduzco@live.com>
2021-04-08 17:48:42 -0500

    Updated working of the landing page, Commented out 'Get Started' button for now. Set Langing page background color to black

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React from "react";
 import logo from "../../img/logocopy.png";
 import LoginButton from "../../components/LoginButton";
 import "./style.css";
 
 const Landing = () => {
-    return (
-        <div>
-            <style>
-                {
-                    "body {background-image: url(https://www.josesgarcia.com/images/adobe.jpeg); background-repeat: no-repeat; background-size: cover; }"
-                }
-            </style>
-            <nav className="navbar navbar-dark fixed-top" style={{ backgroundColor: "rgba(0,0,0,0.8" }}>
-                <a className="navbar-brand" href="/">
-                    <img src={logo} alt="Logo" width="100"></img>
-                </a>
-                <LoginButton>Sign In</LoginButton>
-            </nav>
-            <div className="text-center">
-                <h1 style={{ color: "white", marginTop: "410px" }}>All your streaming in one place.</h1>
-                <h2 style={{ color: "white" }}>Get all your shows and movies under control with A-Lists</h2>
-                <button className="btn btn-light btn-lg mt-3" style={{ fontWeight: "bold" }}>Get Started</button>
-            </div>
-
-        </div>
-    );
-}
+  return (
+    <div>
+      <style>
+        {
+          "body {background-image: url(https://www.josesgarcia.com/images/adobe.jpeg); background-repeat: no-repeat; background-size: cover; background-color:#000000;}"
+        }
+      </style>
+      <nav
+        className="navbar navbar-dark fixed-top"
+        style={{ backgroundColor: "rgba(0,0,0,0.8" }}
+      >
+        <a className="navbar-brand" href="/">
+          <img src={logo} alt="Logo" width="100"></img>
+        </a>
+        <LoginButton>Sign In</LoginButton>
+      </nav>
+      <div className="text-center">
+        <h1 style={{ color: "white", marginTop: "410px" }}>
+          Save it → Watch it
+        </h1>
+        <h2 style={{ color: "white" }}>
+          Get all your shows and movies under control with A-Lists
+        </h2>
+        {/* <button className="btn btn-light btn-lg mt-3" style={{ fontWeight: "bold" }}>Get Started</button> */}
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
