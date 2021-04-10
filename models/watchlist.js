45aef8e1023477d13ec8503ddd3b0b87c05597cc
Janneth Verduzco <janneth_verduzco@live.com>
2021-04-10 14:37:58 -0500

    added models/watchlist.js folder back

---
+++ models/watchlist.js
@@ -0,0 +1,33 @@
+// Dependencies
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Defining Watchlist Schema
+const watchlistSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    view_url: {
+        type: String,
+        required: true
+    },
+    network: {
+        type: String,
+        required: true
+    },
+    poster_url: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
+});
+
+// Passing WatchlistSchema instance to mongoose.model
+const Watchlist = mongoose.model("Watchlist", watchlistSchema);
+
+// Exporting Watchlist Schema
+module.exports = Watchlist;
